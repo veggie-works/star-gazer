72b8bca621a1c6fee87cd19be636f6c898297719
josé <jlc.beker@gmail.com>
2024-02-19 22:44:12 -0300

    changed some things

--- scripts/star.gd
+++ scripts/star.gd
@@ -12,8 +12,5 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body):
 	# check if we have touched the player by checking if the body has a IsPlayer node
-	if body.get_node("IsPlayer"):
-        # add to the colected stars list
-		owner.called_deferred(self)
-		var stars = body.get_node("stars")
-		stars.add_child(self)
+	if body is player:
+		
