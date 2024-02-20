97fa38f1841ebffc1f434e07a964b48f280eba6b
josé <78055979+jossse69@users.noreply.github.com>
2024-02-19 21:57:41 -0300

    added files

---
+++ scripts/star.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body):
+	# check if we have touched the player by checking if the body has a IsPlayer node
+	if body.get_node("IsPlayer"):
+        # add to the colected stars list
+		owner.called_deferred(self)
+		var stars = body.get_node("stars")
+		stars.add_child(self)
