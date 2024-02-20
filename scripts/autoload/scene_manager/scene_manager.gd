dae2e6d70018ada2c46c29bb831c18e8bfa06249
Jason Ngo <jngo102@aim.com>
2024-02-20 16:24:19 -0500

    Fix players duplicating when changing scenes after not saving

--- scripts/autoload/scene_manager/scene_manager.gd
+++ scripts/autoload/scene_manager/scene_manager.gd
@@ -29,5 +29,11 @@ func change_scene(scene_path: String, door_name: String = "") -> void:
 		var doors = get_tree().get_nodes_in_group("doors").filter(func(door): return door.name == door_name)
 		if len(doors) > 0 and doors[0] is Door:
 			doors[0].enter_from.call_deferred()
+	else:
+		var save_points = get_tree().get_nodes_in_group("save_points").filter(func(point): 
+			return (point.global_position - SaveManager.save_data.save_position).length() <= 0.1
+		)
+		if len(save_points) > 0 and save_points[0] is SavePoint:
+			save_points[0].load_save()
 	fader.close()
 	await fader.faded_out
--- scripts/levels/base_level.gd
+++ scripts/levels/base_level.gd
@@ -1,7 +1,7 @@
 ## Base class for all levels in the game
 class_name BaseLevel extends Node
 
-func _ready() -> void:
+func load_save_point() -> void:
 	var save_points: Array[Node] = get_tree().get_nodes_in_group("save_points")
 	for save_point in save_points:
 		if save_point is SavePoint:
