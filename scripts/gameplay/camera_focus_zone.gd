d57d92149326594732e4074ada395d5f799358e8
Jason Ngo <jngo102@aim.com>
2024-03-06 14:15:34 -0500

    Replaced door locations in levels 0 and 2, added metronome track for level 2

--- scripts/gameplay/camera_focus_zone.gd
+++ scripts/gameplay/camera_focus_zone.gd
@@ -10,8 +10,8 @@ extends Node2D
 func _on_trigger_body_entered(body: Player) -> void:
 	GameCamera.locked = true
 	var zoom_tween: Tween = create_tween()
-	zoom_tween.tween_property(GameCamera, "global_position", focus_area.global_position, 0.25)
-	zoom_tween.parallel().tween_property(GameCamera, "zoom", Vector2.ONE * get_viewport_rect().size.x / focus_rect.size.x, 0.25)
+	zoom_tween.tween_property(GameCamera, "global_position", focus_area.global_position, 0.25).set_trans(Tween.TRANS_SINE)
+	zoom_tween.parallel().tween_property(GameCamera, "zoom", Vector2.ONE * get_viewport_rect().size.x / focus_rect.size.x, 0.25).set_trans(Tween.TRANS_SINE)
 	zoom_tween.play()
 
 func _on_trigger_body_exited(body: Player) -> void:
